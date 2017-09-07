282de81c7941d4d9028812ba5822df2a6f4e9458
nruest <ruestn@gmail.com>
2017-09-07 11:41:05 -0400

    url field is presented as a link; Resolves #20
    * Creates helper method for displaying live web urls
    * Uses safe_join instead of join & html_safe; h/t @jcoyne

--- app/helpers/warclight/application_helper.rb
+++ app/helpers/warclight/application_helper.rb
@@ -2,7 +2,12 @@
 
 module Warclight
   ##
-  # Helper for Warclight. This might disappear.
+  # Helper methods for Warclight.
   module ApplicationHelper
+    def link_to_live_web(options = {})
+      safe_join(options[:value].map do |url|
+        link_to(url, url, target: '_blank') << ' 🔗'
+      end, '')
+    end
   end
 end
