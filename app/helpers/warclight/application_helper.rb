922ec979d565ed4ef009e5e1bd047b89145830ae
nruest <ruestn@gmail.com>
2017-10-10 20:20:29 -0400

    Add live http status code check for linking out urls; Resolves #26.

--- app/helpers/warclight/application_helper.rb
+++ app/helpers/warclight/application_helper.rb
@@ -6,7 +6,16 @@ module Warclight
   module ApplicationHelper
     def link_to_live_web(options = {})
       safe_join(options[:value].map do |url|
-        link_to(url, url, target: '_blank') << ' 🔗'
+        begin
+          res = Net::HTTP.get_response(URI(url))
+          if res.code.start_with?('1', '2', '3')
+            link_to(url, url, target: '_blank') << ' 🔗'
+          else
+            options[:value]
+          end
+        rescue
+          options[:value]
+        end
       end, '')
     end
   end
