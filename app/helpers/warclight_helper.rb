66e27b2ddc069c7204d4dd3ebac5be2405533ccb
nruest <ruestn@gmail.com>
2017-10-11 11:32:29 -0400

    Add tests, rename method, update with review feedback.

---
+++ app/helpers/warclight_helper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+##
+# Generic Helpers used in Warclight
+module WarclightHelper
+  def url_to_link(options = {})
+    safe_join(options[:value].map do |url|
+      begin
+        res = Net::HTTP.get_response(URI(url))
+        if res.code.start_with?('1', '2', '3')
+          link_to(url, url, target: '_blank') << ' 🔗'
+        else
+          url + ' (Not available)'
+        end
+      rescue
+        url + ' (Not available)'
+      end
+    end, '')
+  end
+end
