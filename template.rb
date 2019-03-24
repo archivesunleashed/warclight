59e8b65df5ace1be314fe5c623de4ff65b990693
nruest <ruestn@gmail.com>
2019-03-24 17:31:53 -0400

    Update range_limit in template.

--- template.rb
+++ template.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 gem 'warclight', github: 'archivesunleashed/warclight'
-gem 'blacklight_range_limit', '7.0.0.rc2'
+gem 'blacklight_range_limit', '7.0.0'
 
 run 'bundle install'
 
