c7acf4f34e28338edbd360fc8a1ca4f585a244d4
nruest <ruestn@gmail.com>
2019-11-16 20:10:38 -0500

    builds

--- template.rb
+++ template.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 gem 'warclight', github: 'archivesunleashed/warclight'
-gem 'blacklight_range_limit', '7.0.0'
+gem 'blacklight_range_limit', '7.1'
 
 run 'bundle install'
 
