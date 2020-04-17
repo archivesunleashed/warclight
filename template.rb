831b934ca8a2459c40d7a82abd18c7b8641550b7
Nick Ruest <ruestn@gmail.com>
2020-04-16 20:52:33 -0400

    Blacklight range limit 7.8.0 (#115)
    * Update blacklight_range_limit to version 7.8.0
    
    * Remainder of updating range limit
    
    Co-authored-by: depfu[bot] <23717796+depfu[bot]@users.noreply.github.com>

--- template.rb
+++ template.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 gem 'warclight', github: 'archivesunleashed/warclight'
-gem 'blacklight_range_limit', '7.7'
+gem 'blacklight_range_limit', '7.8'
 
 run 'bundle install'
 
